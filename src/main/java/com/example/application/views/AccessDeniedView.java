9d6c00369a1c5e1d731ba7a93b7b97c6388fa3c9
samkkuu <samira.najji@edu.savonia.fi>
2025-04-26 19:25:43 +0300

    Commit

--- src/main/java/com/example/application/views/AccessDeniedView.java
+++ src/main/java/com/example/application/views/AccessDeniedView.java
@@ -3,6 +3,7 @@ package com.example.application.views;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+
 @Route("access-denied")
 public class AccessDeniedView extends VerticalLayout {
 
